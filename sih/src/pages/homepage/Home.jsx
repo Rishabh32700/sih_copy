e016340fe72bda44e624c24954dae7239fe7b48c
Rishabh Sharma <rishabhpep@gmail.com>
2022-07-21 23:42:44 +0530

    commit by rishabh after making home page mail navbar and side navbar

--- sih/src/pages/homepage/Home.jsx
+++ sih/src/pages/homepage/Home.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import NavbarComponent from '../../component/Navbar'
+import VvgnliHomePageSideNavbar from '../../component/VvgnliHomePageSideNavbar'
+
+
 
 const Home = () => {
   return (
     <>
         <div className="home">
             <div className="home__container">
-                Home
+                <NavbarComponent />
+                <div className="home__page__main__content">
+                  <div className="home__page__main__content__container">
+                    <div className="home__page__side__navbar">
+                      <VvgnliHomePageSideNavbar />
+                    </div>
+                    <div className="home__page__rendering__container">
+
+                    </div>
+                  </div>
+                </div>
             </div>
         </div>
     </>
