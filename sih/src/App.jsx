6a10c8a91424cbe94730845f81136d9e3910334a
Rishabh32700 <rishabhpep@gmail.com>
2022-07-23 01:53:41 +0530

    rishabh commit after adding home page little css is gadbad

--- sih/src/App.jsx
+++ sih/src/App.jsx
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import Login from "./pages/Login";
 
 import { Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
-import Home from "./pages/homepage/Home";
+import MainRendering from './components/main_rendering/MainRendering';
+
+export let gData = createContext()
 
 const App = () => {
+
+
+  let [state,setState] =useState(1)
+  let [vvgnli_main_menu_state, set_vvgnli_main_menu_state] = useState("home")
+  let [vvgnli_about_submenu_state, set_vvgnli_about_submenu_state] = useState("about_us")
+  
   return (
     <>
       <div className="app">
         <div className="app__container">
+         
+        <gData.Provider
+            value={{
+              state,
+              setState,
+              vvgnli_main_menu_state,
+              set_vvgnli_main_menu_state,
+              vvgnli_about_submenu_state,
+              set_vvgnli_about_submenu_state,
+            }}
+          >
           <Routes>
-            <Route path="/" element={ <Login />} />
-            <Route path="/signup" element={ <Signup />} />
-            <Route path="/home" element={ <Home />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/home" element={<MainRendering />} />
           </Routes>
+          </gData.Provider>
         </div>
       </div>
     </>
---
+++ sih/src/components/main_rendering/MainRendering.jsx
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react'
+import { gData } from '../../App'
+
+import './mainRendering.css'
+
+import Vvgnli from '../../portals/vvgnli/Vvgnli'
+import SocialMedia from '../../portals/social_media/SocialMedia'
+import Webinars from '../../portals/webinars/Webinars'
+import ThemeLanguageSwitcher from '../theme_language_switcher/ThemeLanguageSwitcher'
+import PortalsMenu from '../portals_menu/PortalsMenu'
+
+
+const MainRendering = () => {
+  let myData = useContext(gData)
+  return (
+    <>
+        <div className="main__rendering">
+            <div className="main__rendering__container">
+            <ThemeLanguageSwitcher />
+            <PortalsMenu />
+                {myData.state===1?<Vvgnli />:''}
+                {myData.state===2?<SocialMedia />:''}
+                {myData.state===3?<Webinars />:''}
+                {myData.state===4?'Btn 4':''}
+                
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default MainRendering
