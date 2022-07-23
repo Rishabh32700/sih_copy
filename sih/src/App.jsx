f39ded9b0c459ed9bcb8a0f6a2da9201ea2f5277
Rishabh32700 <rishabhpep@gmail.com>
2022-07-23 23:12:56 +0530

    rishabh commit after adding webinar page and some css to vvgnli home page

--- sih/src/App.jsx
+++ sih/src/App.jsx
@@ -1,9 +1,14 @@
 import React, { createContext, useState } from "react";
 import Login from "./pages/Login";
 
+
 import { Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
+
 import MainRendering from './components/main_rendering/MainRendering';
+import CallPage from "./portals/webinars/components/CallPage/CallPage";
+import HomePage from "./portals/webinars/components/HomePage/HomePage";
+import NoMatch from "./portals/webinars/components/NoMatch/NoMatch";
 
 export let gData = createContext()
 
@@ -33,6 +38,9 @@ const App = () => {
             <Route path="/" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/home" element={<MainRendering />} />
+            <Route path="/:id" element={<CallPage />} />
+            <Route path="/webinar/homepage" element={<HomePage />} />
+            <Route path="*" element={<NoMatch />} />
           </Routes>
           </gData.Provider>
         </div>
--- sih/src/portals/webinars/Webinars.jsx
+++ sih/src/portals/webinars/Webinars.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./webinars.css";
+import "./App.css";
+import HomePage from "./components/HomePage/HomePage";
+import CallPage from "./components/CallPage/CallPage";
+import NoMatch from "./components/NoMatch/NoMatch";
 
 const Webinars = () => {
   return (
-    <div>Webinars</div>
-  )
-}
+    <>
+      <div className="webinars">
+        <div className="webinars__container">
+          <Routes>
+            <Route path="/:id" element={<CallPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Webinars
+export default Webinars;
