b1afd52062959d30a969c240a8eb7794e6b451bd
Tania <taniachanana02@gmail.com+Tania@users.noreply.github.com>
2022-07-30 15:35:32 +0530

    community sections apis

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql");
 const DATABASE = require("./utilities/createDB");
@@ -5,7 +6,10 @@ const TABLES = require("./utilities/createTables");
 const cred = require("./utilities/credentials");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const multer = require("multer");
+const { s3Uploadv2 } = require("./s3Service");
+const uuid = require("uuid").v4;
+const { S3 } = require("aws-sdk");
 class VVGNLI {
   constructor(port, app) {
     this.port = port;
@@ -25,9 +29,350 @@ class VVGNLI {
     });
   }
 
-  get() {}
+  get() {
+    this.app.get("/api/vvgnli/getPendingPhotos", (req, res) => {
+      const getPendingPhotosSql = "select m.mediaURL, p.userName from media_details m, post_details p where p.mediaId = m.mediaId and p.status = 2 and m.fileType = 1";
+      this.db.query(getPendingPhotosSql, (err, result) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: "some error occured please check it once"
+          });
+        } else {
+          return res.status(200).json({
+            message: "successfully fetched",
+            pendingPhotosArray: result
+          });
+        }
+      })
+    })
+    this.app.get("/api/vvgnli/getPendingVideos", (req, res) => {
+      const getPendingPhotosSql = "select m.mediaURL, p.userName from media_details m, post_details p where p.mediaId = m.mediaId and p.status = 2 and m.fileType = 2";
+      this.db.query(getPendingPhotosSql, (err, result) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: "some error occured please check it once"
+          });
+        } else {
+          return res.status(200).json({
+            message: "successfully fetched",
+            pendingVideosArray: result
+          });
+        }
+      })
+    })
+    this.app.get("/api/vvgnli/getApprovedPhotos", (req, res) => {
+      const getPendingPhotosSql = "select m.mediaURL, p.userName from media_details m, post_details p where p.mediaId = m.mediaId and p.status = 1 and m.fileType = 1";
+      this.db.query(getPendingPhotosSql, (err, result) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: "some error occured please check it once"
+          });
+        } else {
+          return res.status(200).json({
+            message: "successfully fetched",
+            approvedPhotosArray: result
+          });
+        }
+      })
+    })
+    this.app.get("/api/vvgnli/getApprovedVideos", (req, res) => {
+      const getPendingPhotosSql = "select m.mediaURL, p.userName from media_details m, post_details p where p.mediaId = m.mediaId and p.status = 1 and m.fileType = 2";
+      this.db.query(getPendingPhotosSql, (err, result) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: "some error occured please check it once"
+          });
+        } else {
+          return res.status(200).json({
+            message: "successfully fetched",
+            approvedVideosArray: result
+          });
+        }
+      })
+    })
+    this.app.get("/api/vvgnli/getCommentsOnPost", (req, res) => {
+      const mediaId = req.query.mediaId;
+      const getCommentsOnPostSql = "select fullName, commentData from comment_details where mediaId = ?";
+      this.db.query(getCommentsOnPostSql, [mediaId], (err, result) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: "some error occured please check it once"
+          });
+        } else {
+          return res.status(200).json({
+            message: "successfully fetched",
+            commentsArray: result
+          });
+        }
+      })
+    })
+    this.app.get("/api/vvgnli/getLikedPosts", (req, res) => {
+      const userId = req.query.userId;
+      const getLikedPostsSql = "select mediaId from like_details where userId = ?";
+      this.db.query(getLikedPostsSql, [userId], (err, result) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: "some error occured please check it once"
+          });
+        } else {
+          return res.status(200).json({
+            message: "successfully fetched",
+            likedPostsArray: result
+          });
+        }
+      })
+    })
+  }
 
   post() {
+
+    const storage = multer.memoryStorage()
+
+    const fileFilter = (req, file, cb) => {
+      if (file.mimetype.split("/")[0] === 'image' ||
+        file.mimetype.split("/")[0] === 'video') {
+        cb(null, true)
+      } else {
+        cb(new multer.MulterError("LIMIT_UNEXPECTED_FILE"), false)
+      }
+    }
+    // Limits ( fileSize & number of files ) can be changed according to requirements
+    const upload = multer({ storage, fileFilter, limits: { fileSize: 40000000 }, files: 10 });
+    this.app.post("/api/vvgnli/upload", upload.array("file"), async (req, res) => {
+      try {
+        const results = await s3Uploadv2(req.files);
+        console.log("result data ->", results)
+        console.log("result - > ", new Date())
+      } catch (err) {
+        console.log(err);
+      }
+
+      let numberOfFiles = results.length;
+      console.log("num - > ", new Date())
+      this.db.query('Insert into media_details(mediaId, mediaURL, fileName, fileType, currentTimeStamp) values ?',
+        [results.map(result => [result.Key.split("/")[1], result.Location, result.Key.split("-").pop(),
+        (result.Key.split(".").pop() === "jpeg" || result.Key.split(".").pop() === "jpg" || result.Key.split(".").pop() === "png")
+          ? 1 : 2, new Date()])], (err, result) => {
+            console.log("first - > ", new Date())
+            if (err) {
+              console.log(err);
+              return res.status(500).json({
+                message: "some error occured please check it once"
+              });
+            } else {
+              console.log("num of files - > ", numberOfFiles)
+              const getMediaId = "select mediaId from media_details order by currentTimeStamp desc limit ?";
+              this.db.query(getMediaId, [numberOfFiles], (err, result) => {
+                console.log("second - > ", new Date())
+                if (err) {
+                  console.log(err);
+                  return res.status(500).json({
+                    message: "some error occured please check it once"
+                  });
+                } else {
+                  console.log(result)
+                  console.log("thrid - > ", new Date())
+                  return res.status(200).json({
+                    message: "successfully uploaded",
+                    mediaIdArray: result
+                  });
+                }
+              }
+              )
+            }
+
+          })
+    })
+
+    this.app.use((error, req, res, next) => {
+      if (error instanceof multer.MulterError) {
+        if (error.code === 'LIMIT_FILE_SIZE') {
+          return res.status(400).json({
+            message: "file is too large"
+          });
+        }
+
+        if (error.code === 'LIMIT_FILE_COUNT') {
+          return res.status(400).json({
+            message: "file limit reached"
+          });
+        }
+
+        if (error.code === 'LIMIT_UNEXPECTED_FILE') {
+          return res.status(400).json({
+            message: "file must be image or video type"
+          });
+        }
+      }
+    })
+
+    this.app.post("/api/vvgnli/postHandle", (req, res) => {
+      const { userId, mediaIdArray, userName } = req.body;
+      const userRoleSql = 'select userRole from signup_details where userId = ?';
+      this.db.query(userRoleSql, [userId], (err, result) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: "some error occured please check it once"
+          });
+        } else {
+          let userRole = result[0].userRole;
+          let status = 2; // 2 for pending status
+          if (userRole === 1) {
+            status = 1;
+          }
+          this.db.query('Insert into post_details(userId, mediaId, status, userName) values ?',
+            [mediaIdArray.map(mediaIdSingle => [userId, mediaIdSingle.mediaId, status, userName])], (err, result) => {
+
+              if (err) {
+                console.log(err);
+                
+                if (err.code === "ER_DUP_ENTRY") {
+                  res.status(400).send({ success: false, message: 'Duplicate Entry' });
+                }
+                return res.status(500).json({
+                  message: "some error occured please check it once"
+                });
+              } else {
+                return res.status(200).json({
+                  message: "post successfully goes into db"
+
+                });
+              }
+
+            })
+        }
+      })
+    })
+
+    this.app.post("/api/vvgnli/updatePostStatus", (req, res) => {
+      const { mediaId, postStatus, userName } = req.body;
+      if (postStatus === 1) {
+        // update status in post_details from pending to approved
+        const updateStatusSql = "update post_details set status = 1 where mediaId = ?";
+        this.db.query(updateStatusSql, [mediaId], (err, result) => {
+          if (err) {
+            console.log(err);
+            return res.status(500).json({
+              message: "some error occured please check it once"
+            });
+          } else {
+            return res.status(200).json({
+              message: "approved successfully"
+            });
+          }
+        })
+      } else {
+        // delete mediaId from media_details , post_details & also delete objects from s3
+        const postTableDelete = "delete from post_details where mediaId = ?";
+        this.db.query(postTableDelete, [mediaId, userName], (err, result) => {
+          if (err) {
+            console.log(err);
+            return res.status(500).json({
+              message: "some error occured please check it once"
+            });
+          } else {
+            const mediaTableDelete = "delete from media_details where mediaId = ?";
+            this.db.query(mediaTableDelete, [mediaId], (err, result) => {
+              if (err) {
+                console.log(err);
+                return res.status(500).json({
+                  message: "some error occured please check it once"
+                });
+              } else {
+
+                const s3 = new S3({
+                  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+                });
+
+                const params = {
+                  Bucket: process.env.AWS_BUCKET_NAME,
+                  Key: `uploads/${mediaId}`
+                };
+
+                s3.deleteObject(params, (error, data) => {
+                  if (error) {
+                    res.status(500).send(error);
+                  }
+                  return res.status(200).json({
+                    message: "File has been deleted successfully"
+                  });
+                });
+              }
+            })
+          }
+        })
+      }
+    })
+
+    this.app.post("/api/vvgnli/like", (req, res) => {
+      const { userId, mediaId, likeStatus } = req.body;
+      if (likeStatus === 1) {
+        const likeSql = "insert into like_details values(?,?,?)";
+        this.db.query(likeSql, [userId, mediaId, likeStatus], (err, result) => {
+          if (err) {
+            console.log(err);
+            return res.status(500).json({
+              message: "some error occured please check it once"
+            });
+          } else {
+            return res.status(200).json({
+              message: "liked successfully"
+            });
+          }
+        })
+      } else {
+        const dislikeSql = 'delete from like_details where userId = ? and mediaId = ?';
+        this.db.query(dislikeSql, [userId, mediaId], (err, result) => {
+          if (err) {
+            console.log(err);
+            return res.status(500).json({
+              message: "some error occured please check it once"
+            });
+          } else {
+            return res.status(200).json({
+              message: "disliked successfully"
+            });
+          }
+        })
+      }
+    })
+
+    this.app.post("/api/vvgnli/comment", (req, res) => {
+      const { mediaId, commentData, userId } = req.body;
+      let nameSql = "select CONCAT(fname , ' ' , lname) as Name from signup_details where userId = ?";
+      this.db.query(nameSql, [userId], (err, result) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: "some error occured please check it once"
+          });
+        } else {
+          const fullName = result[0].Name;
+          let commentSql = 'insert into comment_details(mediaId, commentData, userId, fullName) values (?,?,?,?)';
+          this.db.query(commentSql, [mediaId, commentData, userId, fullName], (err, result) => {
+            if (err) {
+              console.log(err);
+              return res.status(500).json({
+                message: "some error occured please check it once"
+              });
+            } else {
+              return res.status(200).json({
+                message: "comment successfully"
+              });
+            }
+          })
+        }
+      })
+    });
+
     this.app.post("/api/vvgnli/signup", (req, res) => {
       const {
         fname,
@@ -57,6 +402,9 @@ class VVGNLI {
         (err, result) => {
           if (err) {
             console.log(err);
+            return res.status(500).json({
+              message: "some error occured please check it once"
+            });
           } else {
             console.log("Address record inserted");
             let addressIdFetchSqlQuery =
@@ -65,7 +413,9 @@ class VVGNLI {
             this.db.query(addressIdFetchSqlQuery, (err, result) => {
               if (err) {
                 console.log(err);
-                res.sendStatus(500);
+                return res.status(500).json({
+                  message: "some error occured please check it once"
+                });
               } else {
                 addressId = result[0].addressId;
                 let signSql = `INSERT INTO signup_details (fname, lname, fatherName, gender, dateOfBirth, religion, phoneNumber, userName, emailAddress, userRole, addressId) VALUES (?,?,?,?,?,?,?,?,?,?,?)`;
@@ -86,7 +436,7 @@ class VVGNLI {
                   ],
                   (err, result) => {
                     if (err) {
-                      console.log(err);
+
                       if (err.code === "ER_DUP_ENTRY") {
                         res
                           .status(200)
@@ -102,7 +452,12 @@ class VVGNLI {
                         "SELECT userId FROM signup_details ORDER BY userId DESC LIMIT 1";
                       let userId = 0;
                       this.db.query(userIdFetchSqlQuery, (err, result) => {
-                        if (err) throw err;
+                        if (err) {
+                          console.log(err);
+                          return res.status(500).json({
+                            message: "some error occured please check it once"
+                          });
+                        }
                         else {
                           userId = result[0].userId;
                           const lastSignedIn = new Date();
@@ -119,7 +474,7 @@ class VVGNLI {
                                 res
                                   .status(200)
                                   .send({
-                                    success: false,
+                                    success: true,
                                     message:
                                       "User Account created successfully, Now you can login with you credentials",
                                   });
@@ -148,7 +503,9 @@ class VVGNLI {
         (err, result) => {
           if (err) {
             console.log(err);
-            res.sendStatus(500);
+            return res.status(500).json({
+              message: "some error occured please check it once"
+            });
           } else {
             console.log("fetched id");
             console.log(result);
@@ -170,7 +527,9 @@ class VVGNLI {
                 (err, result) => {
                   if (err) {
                     console.log(err);
-                    res.sendStatus(500);
+                    return res.status(500).json({
+                      message: "some error occured please check it once"
+                    });
                   } else if (result.length == 0) {
                     res
                       .status(200)
@@ -197,7 +556,12 @@ class VVGNLI {
   }
   listen() {
     this.app.listen(this.port, (err) => {
-      if (err) console.log(err);
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          message: "some error occured please check it once"
+        });
+      }
       else console.log(`Server Started On ${this.port}`);
     });
   }
--- backend/utilities/createTables.js
+++ backend/utilities/createTables.js
@@ -13,8 +13,11 @@ class TABLES {
         this.sql = {
             address_details : 'CREATE TABLE IF NOT EXISTS address_details(addressId int auto_increment, addressL1 varchar(70), addressL2 varchar(70), city varchar(20), state varchar(20), country varchar(30), zipCode varchar(6), primary key(addressId))',
             signup_details : 'CREATE TABLE IF NOT EXISTS signup_details(userId int auto_increment, fname varchar(50), lname varchar(50), fatherName varchar(50), gender varchar(20), dateOfBirth date, religion varchar(20), phoneNumber varchar(30), userName varchar(30), emailAddress varchar(30), userRole int, addressId int, PRIMARY KEY (userId), unique key unq_user_details_2 (emailAddress,userRole), CONSTRAINT user_details_ibfk_1 FOREIGN KEY (addressId) REFERENCES address_details (addressId))',
-            login_details : 'CREATE TABLE IF NOT EXISTS login_details(userId int NOT NULL, password varbinary(500) DEFAULT NULL, PRIMARY KEY (userId), CONSTRAINT login_details_ibfk_1 FOREIGN KEY (userId) REFERENCES signup_details (userId))'
-            
+            login_details : 'CREATE TABLE IF NOT EXISTS login_details(userId int NOT NULL, password varbinary(500) DEFAULT NULL, PRIMARY KEY (userId), CONSTRAINT login_details_ibfk_1 FOREIGN KEY (userId) REFERENCES signup_details (userId))',
+            media_details : 'create table IF NOT EXISTS media_details(mediaId varchar(100), mediaURL varchar(200), fileName varchar(100), fileType int, currentTimeStamp TIMESTAMP, primary key(mediaId))',
+            post_details : 'create table IF NOT EXISTS post_details(userId int, mediaId varchar(100), status int, userName varchar(200),PRIMARY KEY (userId,mediaId), CONSTRAINT post_details_ibfk_1 FOREIGN KEY (userId) REFERENCES login_details (userId), CONSTRAINT post_details_ibfk_2 FOREIGN KEY (mediaId) REFERENCES media_details (mediaId))',
+            comment_details : 'create table if not exists comment_details (commentId int auto_increment, mediaId varchar(200), commentData varchar(5000), userId int, fullName varchar(200), primary key(commentId), CONSTRAINT comment_details_ibfk_1 FOREIGN KEY (mediaId) REFERENCES media_details (mediaId), CONSTRAINT comment_details_ibfk_2 FOREIGN KEY (userId) REFERENCES login_details (userId))',
+            like_details : 'create table if not exists like_details (userId int, mediaId varchar(200), likeStatus int, primary key(userId, mediaId), CONSTRAINT like_details_ibfk_1 FOREIGN KEY (userId) REFERENCES login_details (userId), CONSTRAINT like_details_ibfk_2 FOREIGN KEY (mediaId) REFERENCES media_details (mediaId))'
         };
         
     }
