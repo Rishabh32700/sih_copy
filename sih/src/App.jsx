453a61bf9e4c06086820628456c9a8f2a93929af
Rishabh32700 <rishabhpep@gmail.com>
2022-08-06 13:36:10 +0530

    added routing

--- sih/src/App.jsx
+++ sih/src/App.jsx
@@ -1,30 +1,37 @@
 import React, { createContext, useState } from "react";
 import Login from "./pages/Login";
 
+import "./index.css";
 
 import { Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 
-import MainRendering from './components/main_rendering/MainRendering';
+import MainRendering from "./components/main_rendering/MainRendering";
 import HomePage from "./portals/webinars/components/HomePage/HomePage";
+import SocialMedia from "./portals/social_media/SocialMedia";
+import ThemeLanguageSwitcher from "./components/theme_language_switcher/ThemeLanguageSwitcher";
+import PortalsMenu from "./components/portals_menu/PortalsMenu";
+import Webinars from "./portals/webinars/Webinars";
+import ResearchWork from "./portals/research_work/ResearchWork";
+import Dashboard from "./pages/dashboard/Dashboard";
 
-export let gData = createContext()
+export let gData = createContext();
 
 const App = () => {
-
-
-  let [state,setState] =useState(1)
+  let [state, setState] = useState(1);
   let [vvgnli_main_menu_state, set_vvgnli_main_menu_state] = useState("home");
-  let [dashboard_main_menu_state, set_dashboard_main_menu_state] = useState("home");
-  let [vvgnli_about_submenu_state, set_vvgnli_about_submenu_state] = useState("about_us")
-  let [vvgnli_research_submenu_state, set_vvgnli_research_submenu_state] = useState("research")
-  
+  let [dashboard_main_menu_state, set_dashboard_main_menu_state] =
+    useState("home");
+  let [vvgnli_about_submenu_state, set_vvgnli_about_submenu_state] =
+    useState("about_us");
+  let [vvgnli_research_submenu_state, set_vvgnli_research_submenu_state] =
+    useState("research");
+
   return (
     <>
       <div className="app">
         <div className="app__container">
-         
-        <gData.Provider
+          <gData.Provider
             value={{
               state,
               setState,
@@ -38,11 +45,18 @@ const App = () => {
               set_dashboard_main_menu_state,
             }}
           >
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/" element={<MainRendering />} />
-          </Routes>
+            <ThemeLanguageSwitcher />
+            <PortalsMenu />
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/" element={<MainRendering />} />
+              <Route path="/vvgnli" element={<MainRendering />} />
+              <Route path="/community" element={<SocialMedia />} />
+              <Route path="/webinar" element={<Webinars />} />
+              <Route path="/research-section" element={<ResearchWork />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
           </gData.Provider>
         </div>
       </div>
--- sih/src/components/portals_menu/PortalsMenu.jsx
+++ sih/src/components/portals_menu/PortalsMenu.jsx
@@ -18,11 +18,28 @@ import { useNavigate } from "react-router-dom";
 
 import "./portalsMenu.css";
 
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+const settings = [
+  {
+    settingName: "Profile",
+    RoutingLink: "/",
+  },
+  {
+    settingName: "Account",
+    RoutingLink: "/",
+  },
+  {
+    settingName: "Dashboard",
+    RoutingLink: "/dashboard",
+  },
+  {
+    settingName: "Logout",
+    RoutingLink: "/",
+  },
+];
 
 const PortalsMenu = () => {
   let myData = useContext(gData);
-  const navigate=useNavigate();
+  const navigate = useNavigate();
 
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
@@ -43,174 +60,202 @@ const PortalsMenu = () => {
   };
   return (
     <>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/login"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              LOGO
+            </Typography>
 
-          <AppBar position="static">
-            <Container maxWidth="xl">
-              <Toolbar disableGutters>
-                <Typography
-                  variant="h6"
-                  noWrap
-                  component="a"
-                  href="/login"
-                  sx={{
-                    mr: 2,
-                    display: { xs: "none", md: "flex" },
-                    fontFamily: "monospace",
-                    fontWeight: 700,
-                    letterSpacing: ".3rem",
-                    color: "inherit",
-                    textDecoration: "none",
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                <MenuItem
+                  onClick={() => {
+                    myData.setState(1);
                   }}
                 >
-                  LOGO
-                </Typography>
+                  <Typography textAlign="center">VVGNLI</Typography>
+                </MenuItem>
 
-                <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-                  <IconButton
-                    size="large"
-                    aria-label="account of current user"
-                    aria-controls="menu-appbar"
-                    aria-haspopup="true"
-                    onClick={handleOpenNavMenu}
-                    color="inherit"
-                  >
-                    <MenuIcon />
-                  </IconButton>
-                  <Menu
-                    id="menu-appbar"
-                    anchorEl={anchorElNav}
-                    anchorOrigin={{
-                      vertical: "bottom",
-                      horizontal: "left",
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                      vertical: "top",
-                      horizontal: "left",
-                    }}
-                    open={Boolean(anchorElNav)}
-                    onClose={handleCloseNavMenu}
-                    sx={{
-                      display: { xs: "block", md: "none" },
-                    }}
-                  >
-                    <MenuItem
-                      onClick={() => {
-                        myData.setState(1);
-                      }}
-                    >
-                      <Typography textAlign="center">VVGNLI</Typography>
-                    </MenuItem>
-                    <MenuItem
-                      onClick={() => {
-                        myData.setState(2);
-                      }}
-                    >
-                      <Typography textAlign="center">Community</Typography>
-                    </MenuItem>
-                    <MenuItem
-                      onClick={() => {
-                        myData.setState(3);
-                      }}
-                    >
-                      <Typography textAlign="center">Webinar</Typography>
-                    </MenuItem>
-                    <MenuItem
-                      onClick={() => {
-                        myData.setState(4);
-                      }}
-                    >
-                      <Typography textAlign="center">Research Work</Typography>
-                    </MenuItem>
-                  </Menu>
-                </Box>
-
-                <Typography
-                  variant="h5"
-                  noWrap
-                  component="a"
-                  href=""
-                  sx={{
-                    mr: 2,
-                    display: { xs: "flex", md: "none" },
-                    flexGrow: 1,
-                    fontFamily: "monospace",
-                    fontWeight: 700,
-                    letterSpacing: ".3rem",
-                    color: "inherit",
-                    textDecoration: "none",
+                <MenuItem
+                  onClick={() => {
+                    // myData.setState(2);
+                    navigate("community");
+                    console.log("community");
                   }}
                 >
-                  LOGO
-                </Typography>
-                <Box style={{justifyContent:"space-evenly"}} sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-                  <Button
-                    onClick={() => {
-                      myData.setState(1);
-                    }}
-                    sx={{ my: 2, color: "white", display: "block" }}
-                  >
-                    VVGNLI
-                  </Button>
-                  <Button
-                    onClick={() => {
-                      myData.setState(2);
-                    }}
-                    sx={{ my: 2, color: "white", display: "block" }}
-                  >
-                    Community
-                  </Button>
-                  <Button
-                    onClick={() => {
-                      myData.setState(3);
-                    }}
-                    sx={{ my: 2, color: "white", display: "block" }}
-                  >
-                    Webinar
-                  </Button>
-                  <Button
-                    onClick={() => {
-                      myData.setState(4);
-                    }}
-                    sx={{ my: 2, color: "white", display: "block" }}
-                  >
-                    Research Work
-                  </Button>
-                </Box>
-                {console.log(myData.state)}
+                  <Typography textAlign="center">Community</Typography>
+                </MenuItem>
 
-                <Box sx={{ flexGrow: 0 }}>
-                  <Tooltip title="Open settings">
-                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                      <Avatar alt="R" src="/static/images/avatar/2.jpg" />
-                    </IconButton>
-                  </Tooltip>
-                  <Menu
-                    sx={{ mt: "45px" }}
-                    id="menu-appbar"
-                    anchorEl={anchorElUser}
-                    anchorOrigin={{
-                      vertical: "top",
-                      horizontal: "right",
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                      vertical: "top",
-                      horizontal: "right",
-                    }}
-                    open={Boolean(anchorElUser)}
-                    onClose={handleCloseUserMenu}
+                <MenuItem
+                  onClick={() => {
+                    // myData.setState(3);
+                    navigate("webinar");
+                    console.log("webinar");
+                  }}
+                >
+                  <Typography textAlign="center">Webinar</Typography>
+                </MenuItem>
+
+                <MenuItem
+                  onClick={() => {
+                    // myData.setState(4);
+                    navigate("research-section");
+                    console.log("research-section");
+                  }}
+                >
+                  <Typography textAlign="center">Research Work</Typography>
+                </MenuItem>
+              </Menu>
+            </Box>
+
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href=""
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              LOGO
+            </Typography>
+            <Box
+              style={{ justifyContent: "space-evenly" }}
+              sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
+            >
+              <Button
+                onClick={() => {
+                  myData.setState(1);
+                }}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                VVGNLI
+              </Button>
+              <Button
+                onClick={() => {
+                  // myData.setState(2);
+                  navigate("community");
+                  console.log("community");
+                }}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                Community
+              </Button>
+              <Button
+                onClick={() => {
+                  myData.setState(3);
+                  navigate("webinar");
+                  console.log("webinar");
+                }}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                Webinar
+              </Button>
+              <Button
+                onClick={() => {
+                  myData.setState(4);
+                  navigate("research-section");
+                  console.log("research-section");
+                }}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                Research Work
+              </Button>
+            </Box>
+            {console.log(myData.state)}
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="R" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting, index) => (
+                  <MenuItem
+                    key={setting.settingName}
+                    onClick={handleCloseUserMenu}
                   >
-                    {settings.map((setting,index) => (
-                      <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                        <Typography textAlign="center" onClick={()=>myData.setState(index+5)}>{setting}</Typography>
-                      </MenuItem>
-                    ))}
-                  </Menu>
-                </Box>
-              </Toolbar>
-            </Container>
-          </AppBar>
+                    <Typography
+                      textAlign="center"
+                      onClick={() => {
+                        myData.setState(index + 5);
+                        navigate(setting.RoutingLink);
+                      }}
+                    >
+                      {setting.settingName}
+                    </Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
     </>
   );
 };
