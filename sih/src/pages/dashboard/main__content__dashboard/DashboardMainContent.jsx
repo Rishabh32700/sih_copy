a52ae62f9e6c247e7fe7c7c0e35196a37ce64bf9
Nand Kumar <nand.kumar@bitcs.in>
2022-08-10 12:43:50 +0530

    Nand Commits Added Dynamic role fetching in Dashboard

--- sih/src/pages/dashboard/main__content__dashboard/DashboardMainContent.jsx
+++ sih/src/pages/dashboard/main__content__dashboard/DashboardMainContent.jsx
@@ -1,35 +1,65 @@
-import React, { useContext } from 'react'
-import { gData } from '../../../App'
-import './DashboardMainContent.css'
-import DashboardHome from '../pages/home/DashboardHome'
+import React, { useContext } from "react";
+import { gData } from "../../../App";
+import "./DashboardMainContent.css";
+import DashboardHome from "../pages/home/DashboardHome";
 // import DashboardCommunity from '../pages/community/DashboardCommunity'
-import DashboardResearch from '../pages/research/DashboardResearch'
-import DashboardWebinar from '../pages/webinars/DashboardWebinar'
-import DashboardCommunityVideos from '../pages/community/DashboardCommunity'
-import DashboardCommunityImages from '../pages/community/DashboardCommunityImages'
-import DashboardModalImages from '../pages/community/DashboardModalImages'
-
+import DashboardResearch from "../pages/research/DashboardResearch";
+import DashboardWebinar from "../pages/webinars/DashboardWebinar";
+import DashboardCommunityVideos from "../pages/community/DashboardCommunity";
+import DashboardCommunityImages from "../pages/community/DashboardCommunityImages";
+import DashboardModalImages from "../pages/community/DashboardModalImages";
 
 const DashboardMainContent = () => {
-
   const myGlobalDataForDashboardMainMenu = useContext(gData);
-  const isAdmin=true;
+  const user = JSON.parse(sessionStorage.getItem("user"));
+  console.log(user.role);
+  const isAdmin = user.role === 1 ? true : false;
 
   return (
     <>
-            <div className="dashboard__main__content">
-                <div className="dashboard__main__content__container">
-                  {myGlobalDataForDashboardMainMenu.dashboard_main_menu_state === 'home'?<DashboardHome  isAdmin={isAdmin}/>:''}
-                  {myGlobalDataForDashboardMainMenu.dashboard_main_menu_state === 'images'?<DashboardCommunityImages  isAdmin={isAdmin}/>:''}
-                  {myGlobalDataForDashboardMainMenu.dashboard_main_menu_state === 'videos'?<DashboardCommunityVideos  isAdmin={isAdmin}/>:''}
-                  {/* {myGlobalDataForDashboardMainMenu.dashboard_main_menu_state === 'community'?<DashboardCommunity />:''} */}
-                  {myGlobalDataForDashboardMainMenu.dashboard_main_menu_state === 'research'?<DashboardResearch  isAdmin={isAdmin}/>:''}
-                  {myGlobalDataForDashboardMainMenu.dashboard_main_menu_state === 'webinar'?<DashboardWebinar  isAdmin={isAdmin}/>:''}
-                  {myGlobalDataForDashboardMainMenu.dashboard_main_menu_state === 'imageModal'?<DashboardModalImages  isAdmin={isAdmin}/>:''}
-                </div>
-            </div>
-        </>
-  )
-}
+      <div className="dashboard__main__content">
+        <div className="dashboard__main__content__container">
+          {myGlobalDataForDashboardMainMenu.dashboard_main_menu_state ===
+          "home" ? (
+            <DashboardHome isAdmin={isAdmin} />
+          ) : (
+            ""
+          )}
+          {myGlobalDataForDashboardMainMenu.dashboard_main_menu_state ===
+          "images" ? (
+            <DashboardCommunityImages isAdmin={isAdmin} />
+          ) : (
+            ""
+          )}
+          {myGlobalDataForDashboardMainMenu.dashboard_main_menu_state ===
+          "videos" ? (
+            <DashboardCommunityVideos isAdmin={isAdmin} />
+          ) : (
+            ""
+          )}
+          {/* {myGlobalDataForDashboardMainMenu.dashboard_main_menu_state === 'community'?<DashboardCommunity />:''} */}
+          {myGlobalDataForDashboardMainMenu.dashboard_main_menu_state ===
+          "research" ? (
+            <DashboardResearch isAdmin={isAdmin} />
+          ) : (
+            ""
+          )}
+          {myGlobalDataForDashboardMainMenu.dashboard_main_menu_state ===
+          "webinar" ? (
+            <DashboardWebinar isAdmin={isAdmin} />
+          ) : (
+            ""
+          )}
+          {myGlobalDataForDashboardMainMenu.dashboard_main_menu_state ===
+          "imageModal" ? (
+            <DashboardModalImages isAdmin={isAdmin} />
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default DashboardMainContent
+export default DashboardMainContent;
